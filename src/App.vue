c67b15f468848d10f4c9abd1cbb8a68b5e0d53ba
ydanielaosorio <daniela.osorio@imexhs.com>
2022-05-25 10:07:31 -0500

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WorkList msg="WorkList"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import WorkList from './components/WorkList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WorkList
   }
 }
 </script>
---
+++ src/components/WorkList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="workList">
+    <h1>{{msg}}</h1>
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
